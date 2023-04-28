6620b795d2b9cfc1e92c750e64c322c31edb4b11
samirahmad5445 <samirahmad5445@gmail.com>
2023-04-28 17:18:14 +0200

    add theme toggle to navbar

---
+++ src/js/toggle.js
@@ -0,0 +1,45 @@
+const storageKey = "my-custom-theme";
+
+const getColorPreference = () => {
+  if (localStorage.getItem(storageKey)) return localStorage.getItem(storageKey);
+  else
+    return window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light";
+};
+
+let theme = getColorPreference();
+
+const setPreference = () => {
+  localStorage.setItem(storageKey, theme);
+  reflectPreference();
+};
+
+const reflectPreference = () => {
+  document.firstElementChild.setAttribute("data-theme", theme);
+
+  document.querySelector("#theme-toggle")?.setAttribute("aria-label", theme);
+};
+
+reflectPreference();
+
+window.onload = () => {
+  // set on load so screen readers can get the latest value on the button
+  reflectPreference();
+
+  // now this script can find and listen for clicks on the control
+  document.querySelector("#toggle").addEventListener("click", onClick);
+};
+
+const onClick = () => {
+  theme = theme === "light" ? "dark" : "light";
+
+  setPreference();
+};
+
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", ({ matches: isDark }) => {
+    theme = isDark ? "dark" : "light";
+    setPreference();
+  });
